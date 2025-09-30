62a253b4b74e1765aa05c66102a69a0eab066e4d
ngviet291 <vietnguyen29105@gmail.com>
2025-09-30 12:01:00 +0700

    chore(layout):Them HomePage

---
+++ src/main/java/iuh/fit/se/group1/util/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iuh.fit.se.group1.util;
+
+/**
+ *
+ * @author vietn
+ */
+public class Constants {
+    public static int HEIGHT_FRAME = 880;
+    public static int WIDTH_FRAME = 1246;
+}
