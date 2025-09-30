18e1a785a8ef88ad1d4b420cfc2ebca74239747d
ngphthinh <nguyenphuocthinh0710@gmail.com>
2025-09-30 16:27:11 +0700

    Gop file Constants.java

--- src/main/java/iuh/fit/se/group1/util/Constants.java
+++ src/main/java/iuh/fit/se/group1/util/Constants.java
@@ -1,32 +1,17 @@
-<<<<<<< HEAD
+
 package iuh.fit.se.group1.util;
 
 import java.awt.*;
 
 public class Constants {
-    public static final Color FOREGROUND_COLOR_MENU = new Color(77,134,168);
+    public static final Color FOREGROUND_COLOR_MENU = new Color(77, 134, 168);
 
     public static final Color BACKGROUND_COLOR_MENU = new Color(255, 255, 255);
 
     public static final Color COLOR_ICON_MENU = Color.decode("#FF6C03");
 
-    public static final Color LINE_SUBMENU = new Color(181,181,181);
+    public static final Color LINE_SUBMENU = new Color(181, 181, 181);
 
     public static final int HEIGHT_FRAME = 800;
     public static final int WIDTH_FRAME = 1300;
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package iuh.fit.se.group1.util;
-
-/**
- *
- * @author vietn
- */
-public class Constants {
-    public static int HEIGHT_FRAME = 880;
-    public static int WIDTH_FRAME = 1246;
->>>>>>> fb36c97f0113ac773c9363b0a38425e23defcb54
 }
