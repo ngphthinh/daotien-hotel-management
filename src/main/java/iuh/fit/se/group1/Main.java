726e666117906c2f5edd96b3c37fec3e5ef62708
ngviet291 <vietnguyen29105@gmail.com>
2025-10-07 17:59:11 +0700

    feat(swing):Them Swing

--- src/main/java/iuh/fit/se/group1/Main.java
+++ src/main/java/iuh/fit/se/group1/Main.java
@@ -2,12 +2,16 @@ package iuh.fit.se.group1;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 
+import iuh.fit.se.group1.ui.layout.LoginUI;
+import javax.swing.SwingUtilities;
+
+
 
 
 
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-     
+        
     }
 }
--- src/main/java/iuh/fit/se/group1/ui/layout/LoginUI.java
+++ src/main/java/iuh/fit/se/group1/ui/layout/LoginUI.java
@@ -141,10 +141,10 @@ public class LoginUI extends javax.swing.JFrame {
                 .addGap(32, 32, 32))
         );
 
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 70, 350, 360));
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 50, 350, 360));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/background.png"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-60, -60, 800, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
