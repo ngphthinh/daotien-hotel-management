947859197fc6d59e19b1932aaa79f61f100d2d48
ngphthinh <nguyenphuocthinh0710@gmail.com>
2025-11-03 12:52:02 +0700

    s

--- src/main/java/iuh/fit/se/group1/Main.java
+++ src/main/java/iuh/fit/se/group1/Main.java
@@ -12,10 +12,6 @@ import java.time.LocalDate;
 import java.util.concurrent.CountDownLatch;
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-
-
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         try {
